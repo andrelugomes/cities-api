bea442627d4ac7b34a0c85c627261b90fa28f312
André Luis Gomes <andrelugomes@gmail.com>
2020-07-08 21:55:56 -0300

    Nearby service Update ReadMe Refactoring Rest APIs

--- src/main/java/com/github/andrelugomes/cities/resources/CityResource.java
+++ src/main/java/com/github/andrelugomes/cities/resources/CityResource.java
@@ -2,6 +2,8 @@ package com.github.andrelugomes.cities.resources;
 
 import com.github.andrelugomes.cities.entities.City;
 import com.github.andrelugomes.cities.repositories.CityRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("cities")
 public class CityResource {
 
+  private static Logger log = LoggerFactory.getLogger(CityResource.class);
   private final CityRepository repository;
 
   public CityResource(final CityRepository repository) {
     this.repository = repository;
   }
 
-  /* 1st
-  @GetMapping
-  public List<City> cities() {
-      return repository.findAll();
-  }*/
-
-  // 2nd - Pageable
   @GetMapping
   public Page<City> cities(final Pageable page) {
+    log.info("cities, {}", page);
     return repository.findAll(page);
   }
 }
